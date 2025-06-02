76c93572df3ff10d2d42e386befd5b0719410fe3
willymusyoka <willykyeni@gmail.com>
2025-06-02 16:40:55 +0300

    Adds Dodger-right

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+//moving dodger left
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+//moving dodger right
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left <360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+  // Add condition for right arrow key
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
